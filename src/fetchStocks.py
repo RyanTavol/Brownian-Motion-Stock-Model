615611bb5285bf232399668bf61c3fbc8aa4a19b
RyanTavol <121977925+RyanTavol@users.noreply.github.com>
2024-04-24 18:07:55 -0400

    fixed code so that I can get the whole stock history with no possibility of errors

--- src/fetchStocks.py
+++ src/fetchStocks.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
+from pandas.tseries.offsets import BDay
 
 class StockData:
     # For now I am going to require that a start date and end date are required fields
@@ -11,35 +12,29 @@ class StockData:
         # Do some basic logic about checking if ticker, start, and end are valid
         # But for now let's assume they are all fine
         self.stock_data_df = pd.DataFrame(self.__fetchDailyStockData(ticker, start_date, end_date))
-        self.start_date = start_date
-
-        self.start_date = start_date
-        self.end_date = end_date
         
-        # self.start_date = datetime.strptime(self.stock_data_df.index[0].strftime('%Y-%m-%d'), '%Y-%m-%d')
-        # self.end_date = datetime.strptime(self.stock_data_df.index[-1].strftime('%Y-%m-%d'), '%Y-%m-%d')
-
-        # self.end_date += timedelta(days=1)
+        self.start_date = datetime.strptime(self.stock_data_df.index[0].strftime('%Y-%m-%d'), '%Y-%m-%d')
+        self.end_date = datetime.strptime(self.stock_data_df.index[-1].strftime('%Y-%m-%d'), '%Y-%m-%d')
+        self.end_date += BDay(1)
 
         self.market_data_df = pd.DataFrame(self.__fetchDailyStockData("^GSPC", self.start_date, self.end_date))
         self.beta = self.__calcBeta(self.stock_data_df, self.market_data_df)
 
         # risk free rate is calculated typically using the bond price
-        # market return is on average the return of the SP500 (I will only concern myself with stocks in this market)
         self.risk_free_rate = 0.2
+        # market return is on average the return of the SP500 (I will only concern myself with stocks in this market)
         self.market_return = 0.8
         
 
     def __fetchDailyStockData(self, ticker, start_date = None, end_date = None):
-        if(start_date != None and end_date != None):
-            return yf.download(ticker, start_date, end_date)
-        if(start_date != None and end_date == None):
-            return yf.download(ticker, start = start_date)
-        if(start_date == None and end_date != None):
-            return yf.download(ticker, end = end_date)
-        
-        # Returns the entire history of stock data
-        return yf.download(ticker)
+        if start_date and end_date:
+            return yf.download(ticker, start=start_date, end=end_date)
+        elif start_date:
+            return yf.download(ticker, start=start_date)
+        elif end_date:
+            return yf.download(ticker, end=end_date)
+        else:
+            return yf.download(ticker)
     
     def __calcBeta(self, stock_data, market_data):
         # Calculate daily returns for stock and market
@@ -68,9 +63,3 @@ class StockData:
 
         # Get the closing price for the most recent date
         return self.stock_data_df.loc[most_recent_date, 'Close']
-
-# apple = StockData("AAPL", '2023-01-01', '2023-12-31')
-# print(apple.stock_data_df)
-# print(apple.getAllForDate("2023-01-05"))
-# print(len(apple.getClosingPrices()))
-# print(apple.getMostCurrentPrice())
